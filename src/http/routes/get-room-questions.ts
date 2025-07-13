fd9c8311c73d1e4cb3e83869d64927fdef1a661a
Kevin Souza <ksouza.dev@gmail.com>
2025-07-12 21:17:18 -0300

    adding upload audio route, transcription and gemini service

--- src/http/routes/get-room-questions.ts
+++ src/http/routes/get-room-questions.ts
@@ -1,30 +1,34 @@
-import { eq, desc } from 'drizzle-orm'
+import { desc, eq } from 'drizzle-orm'
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
+import z from 'zod/v4'
 import { db } from '../../db/connection.ts'
 import { schema } from '../../db/schema/index.ts'
-import z from 'zod/v4'
-import { questions } from '../../db/schema/questions.ts'
 
 export const getRoomQuestions: FastifyPluginCallbackZod = (app) => {
-  app.get('/rooms/:roomId/questions', {
-    schema: {
-      params: z.object({
-        roomId: z.string()
-      })
+  app.get(
+    '/rooms/:roomId/questions',
+    {
+      schema: {
+        params: z.object({
+          roomId: z.string(),
+        }),
+      },
+    },
+    async (request) => {
+      const { roomId } = request.params
+
+      const result = await db
+        .select({
+          id: schema.questions.id,
+          question: schema.questions.question,
+          answer: schema.questions.answer,
+          createdAt: schema.questions.createdAt,
+        })
+        .from(schema.questions)
+        .where(eq(schema.questions.roomId, roomId))
+        .orderBy(desc(schema.questions.createdAt))
+
+      return result
     }
-  }, async (request) => {
-    const { roomId } = request.params
-
-    const result = await db.select({
-      id: schema.questions.id,
-      questions: schema.questions.question,
-      answer: schema.questions.answer,
-      createdAt: schema.questions.createdAt
-    }).from(schema.questions).where(
-      eq(schema.questions.roomId, roomId)
-    ).orderBy(desc(schema.questions.createdAt))
-
-    return result
-  })
-
+  )
 }
